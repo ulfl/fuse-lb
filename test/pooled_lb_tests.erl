1291b67f22b57b3e7865965e00f8c2a652675612
Ulf Leopold <336dev@gmail.com>
2014-04-13 12:06:21 +0200

    Additional tests.

--- test/pooled_lb_tests.erl
+++ test/pooled_lb_tests.erl
@@ -32,6 +32,57 @@ pool_in_front_of_lb_test() ->
   ?ae(2, fuse_pool:queued_work(P)),
   w(6).
 
+lb_in_front_of_pools_test() ->
+  Probe = fun(X) -> {available, X} end,
+  Tmos = [1000],
+  Log = fun(F, A) -> ?debugFmt(F, A) end,
+  {ok, Lb} = fuse_lb:start_link([{b1, Tmos, Probe}, {b2, Tmos, Probe}],
+                                round_robin, Log),
+  {ok, P} = fuse_pool:start_link([{w1, Tmos, Probe}, {w2, Tmos, Probe}], Log),
+
+  Work = fun(D) -> timer:sleep(1000), {available, D} end,
+  PoolCall = fun(Box) ->
+                 {available, {Box, fuse_pool:call(P, Work)}}
+             end,
+
+  ?ae({ok, {b1, w1}}, fuse_lb:call(Lb, PoolCall)),
+  ?ae({ok, {b2, w1}}, fuse_lb:call(Lb, PoolCall)),
+  ?ae({ok, {b1, w1}}, fuse_lb:call(Lb, PoolCall)),
+  ?ae({ok, {b2, w1}}, fuse_lb:call(Lb, PoolCall)).
+
+lb_in_front_of_pools_async_test() ->
+  Probe = fun(X) -> {available, X} end,
+  Tmos = [1000],
+  Log = fun(F, A) -> ?debugFmt(F, A) end,
+  {ok, P1} = fuse_pool:start_link([{p1w1, Tmos, Probe}, {p1w2, Tmos, Probe}],
+                                  Log),
+  {ok, P2} = fuse_pool:start_link([{p2w1, Tmos, Probe}, {p2w2, Tmos, Probe}],
+                                  Log),
+  {ok, Lb} = fuse_lb:start_link([{{b1, P1}, Tmos, Probe},
+                                 {{b2, P2}, Tmos, Probe}],
+                                round_robin, Log),
+
+  Work = fun(D) -> timer:sleep(500), {available, D} end,
+  PoolCall = fun({Box, Pool}) ->
+                 {available, {Box, fuse_pool:call(Pool, Work)}}
+             end,
+
+  s(fun() -> ?ae({ok, {b1, p1w1}}, fuse_lb:call(Lb, PoolCall)) end),
+  timer:sleep(50),
+  s(fun() -> ?ae({ok, {b2, p2w1}}, fuse_lb:call(Lb, PoolCall)) end),
+  s(fun() -> ?ae({ok, {b1, p1w2}}, fuse_lb:call(Lb, PoolCall)) end),
+  timer:sleep(50),
+  s(fun() -> ?ae({ok, {b2, p2w2}}, fuse_lb:call(Lb, PoolCall)) end),
+  timer:sleep(50),
+  ?ae(0, fuse_pool:available_fuses(P1)),
+  ?ae(0, fuse_pool:available_fuses(P2)),
+  s(fun() -> ?ae({ok, {b1, p1w1}}, fuse_lb:call(Lb, PoolCall)) end),
+  s(fun() -> ?ae({ok, {b2, p2w1}}, fuse_lb:call(Lb, PoolCall)) end),
+  timer:sleep(50),
+  ?ae(1, fuse_pool:queued_work(P1)),
+  ?ae(1, fuse_pool:queued_work(P2)),
+  w(6).
+
 %%%_* Helpers ==========================================================
 s(Fun) ->
   Pid = spawn_link(Fun),

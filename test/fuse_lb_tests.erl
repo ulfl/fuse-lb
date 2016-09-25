f01822e9316204d4796846293e603351e1b00aa5
Ulf Leopold <336dev@gmail.com>
2016-09-25 17:35:59 +0200

    Add more margin to test.

--- test/fuse_lb_tests.erl
+++ test/fuse_lb_tests.erl
@@ -112,17 +112,17 @@ round_robin_backoff_test() ->
   ?ae({ok, 3}, fuse_lb:call(Lb, fun(UserData) -> {available, UserData} end)),
   ?ae({ok, 1}, fuse_lb:call(Lb, fun(UserData) -> {unavailable, UserData} end)),
 
-  timer:sleep(22),
+  timer:sleep(23),
   ?ae(3, Cnt(get)),
   ?ae({ok, 2}, fuse_lb:call(Lb, fun(UserData) -> {available, UserData} end)),
   ?ae({ok, 3}, fuse_lb:call(Lb, fun(UserData) -> {available, UserData} end)),
 
-  timer:sleep(52),
+  timer:sleep(53),
   ?ae(4, Cnt(get)),
   ?ae({ok, 2}, fuse_lb:call(Lb, fun(UserData) -> {available, UserData} end)),
   ?ae({ok, 3}, fuse_lb:call(Lb, fun(UserData) -> {available, UserData} end)),
 
-  timer:sleep(502),
+  timer:sleep(503),
   ?ae(5, Cnt(get)),
   ?ae({ok, 2}, fuse_lb:call(Lb, fun(UserData) -> {available, UserData} end)),
   ?ae({ok, 3}, fuse_lb:call(Lb, fun(UserData) -> {available, UserData} end)),
